66d2b48b70f296135ab810eae4e61e85934c8416
Daphnee Jeune <daphneejeune.dev@gmail.com>
2024-11-23 15:58:23 -0600

    WIP work on more details modal

--- src/sections/patient/patients-popover-menu.tsx
+++ src/sections/patient/patients-popover-menu.tsx
@@ -1,4 +1,5 @@
-import { Popover, MenuList, MenuItem, menuItemClasses } from "@mui/material"
+import { useState } from 'react';
+import { Popover, MenuList, MenuItem, menuItemClasses } from '@mui/material';
 import { Iconify } from 'src/components/iconify';
 
 type PatientPopoverMenuProps = {
@@ -7,47 +8,55 @@ type PatientPopoverMenuProps = {
   handleEditRow: (row: any) => void;
   handleDeletePatientRecord: (id: string) => void;
   rowId: string;
-}
-const PatientsPopoverMenu = ({ openPopover, handleClosePopover, handleEditRow, handleDeletePatientRecord, rowId }: PatientPopoverMenuProps) => {
+};
+const PatientsPopoverMenu = ({
+  openPopover,
+  handleClosePopover,
+  handleEditRow,
+  handleDeletePatientRecord,
+  rowId,
+}: PatientPopoverMenuProps) => {
+  const [showMoreDetails, setShowMoreDetails] = useState(false);
   return (
     <Popover
-        open={!!openPopover}
-        anchorEl={openPopover}
-        onClose={handleClosePopover}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={!!openPopover}
+      anchorEl={openPopover}
+      onClose={handleClosePopover}
+      anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+    >
+      <MenuList
+        disablePadding
+        sx={{
+          p: 0.5,
+          gap: 0.5,
+          width: 140,
+          display: 'flex',
+          flexDirection: 'column',
+          [`& .${menuItemClasses.root}`]: {
+            px: 1,
+            gap: 2,
+            borderRadius: 0.75,
+            [`&.${menuItemClasses.selected}`]: { bgcolor: 'action.selected' },
+          },
+        }}
       >
-        <MenuList
-          disablePadding
-          sx={{
-            p: 0.5,
-            gap: 0.5,
-            width: 140,
-            display: 'flex',
-            flexDirection: 'column',
-            [`& .${menuItemClasses.root}`]: {
-              px: 1,
-              gap: 2,
-              borderRadius: 0.75,
-              [`&.${menuItemClasses.selected}`]: { bgcolor: 'action.selected' },
-            },
-          }}
-        >
-          <MenuItem onClick={handleClosePopover}>
-            <Iconify icon="ic:baseline-add" />
-            More details
-          </MenuItem>
-          <MenuItem onClick={handleEditRow}>
-            <Iconify icon="solar:pen-bold" />
-            Edit
-          </MenuItem>
-          <MenuItem onClick={() => handleDeletePatientRecord(rowId)} sx={{ color: 'error.main' }}>
-            <Iconify icon="solar:trash-bin-trash-bold" />
-            Delete
-          </MenuItem>
-        </MenuList>
-      </Popover>
-  )
-}
+        <MenuItem onClick={() => setShowMoreDetails(true)}>
+          <Iconify icon="ic:baseline-add" />
+          More details
+        </MenuItem>
+        <MenuItem onClick={handleEditRow}>
+          <Iconify icon="solar:pen-bold" />
+          Edit
+        </MenuItem>
+        <MenuItem onClick={() => handleDeletePatientRecord(rowId)} sx={{ color: 'error.main' }}>
+          <Iconify icon="solar:trash-bin-trash-bold" />
+          Delete
+        </MenuItem>
+      </MenuList>
+      {showMoreDetails && alert(`More details`)}
+    </Popover>
+  );
+};
 
-export default PatientsPopoverMenu
+export default PatientsPopoverMenu;
