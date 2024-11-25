b4e456891d41f16bed52f0166927e831d458d6ba
Daphnee Jeune <daphneejeune.dev@gmail.com>
2024-11-25 16:33:26 -0600

    improve error handling on empty table

--- src/sections/patient/table-no-data.tsx
+++ src/sections/patient/table-no-data.tsx
@@ -1,5 +1,6 @@
 import type { TableRowProps } from '@mui/material/TableRow';
-import { Box, TableRow, TableCell, Typography } from '@mui/material';
+import { Box, TableRow, TableCell, Typography, Button } from '@mui/material';
+import useFetchPatients from '../patient/view/useFetchPatients';
 
 type EmptyTableProps = TableRowProps & {
   searchQuery: string;
@@ -7,14 +8,16 @@ type EmptyTableProps = TableRowProps & {
 };
 
 const EmptyTable = ({ searchQuery, isTableEmpty }: EmptyTableProps) => {
+  const { error, refetch } = useFetchPatients();
   const tableCopy = isTableEmpty
     ? "There are no patient records currently. Click on the 'New patient' button to create one!"
-    : `No results found for '${searchQuery}'. Try typing a different entry.`;
+    : error ? `There was an error loading the data. Please try again!` : `No results found for '${searchQuery}'. Try typing a different entry.`;
   return (
     <TableRow>
       <TableCell align="center" colSpan={7}>
         <Box sx={{ py: 15, textAlign: 'center' }}>
           <Typography sx={{ mb: 1 }}>{tableCopy}</Typography>
+        {error && <Button onClick={refetch}>Refetch</Button>}
         </Box>
       </TableCell>
     </TableRow>
